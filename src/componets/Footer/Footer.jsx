07c1a6ab72de752d5a1a09528a968f01c4c6ec38
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-03-06 13:29:35 +0200

    create footer

--- src/componets/Footer/Footer.jsx
+++ src/componets/Footer/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Container from "../Container";
+import css from "./Footer.module.css";
 
 const Footer = () => {
-  return <footer>footer</footer>;
+  return (
+    <footer className={css.footer}>
+      <Container>
+        <div className={css.wrapper}>
+          <p className={css.tel}>718-669-1100</p>
+          <p className={css.address}>94-38 59th Ave Elmhurst, NY 11373</p>
+        </div>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
