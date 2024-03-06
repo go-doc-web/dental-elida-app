2ac86592fd3ea77d421b4cd6f925c496d20d71ff
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-03-06 14:40:33 +0200

    feat: добавлена страница TMJPage

---
+++ src/app/orthodontist/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const OrthodontistPage = () => {
+  return <h1>Orthodontist</h1>;
+};
+
+export default OrthodontistPage;
---
+++ src/app/tmj/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TMJPage = () => {
+  return <h1>TMJ Page</h1>;
+};
+
+export default TMJPage;
