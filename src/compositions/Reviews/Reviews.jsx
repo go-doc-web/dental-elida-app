03ac5b84ac04c85970d10be6a95b764f48accf85
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-03-11 22:49:35 +0200

    create pagination on thre page Reviews

--- src/compositions/Reviews/Reviews.jsx
+++ src/compositions/Reviews/Reviews.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Pagination } from 'antd';
 import css from './Reviews.module.css';
 import Heading from '@/componets/Heading';
 import ReviewsList from './ReviewsList';
 import Line from '@/componets/Line';
+
+const onShowSizeChange = (current, pageSize) => {
+  console.log(current, pageSize);
+};
 
 const Reviews = () => {
   return (
@@ -11,8 +16,15 @@ const Reviews = () => {
         Reviews
       </Heading>
       <Line className={css.line} />
-
       <ReviewsList />
+      <div className={css.pagination}>
+        <Pagination
+          showSizeChanger
+          // onShowSizeChange={onShowSizeChange}
+          defaultCurrent={3}
+          total={500}
+        />
+      </div>
     </>
   );
 };
