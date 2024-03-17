70faad7898aa3b78fdf684d1be18d0c40b71cf95
Go.Doc <97858843+OlegDoc@users.noreply.github.com>
2024-03-17 15:13:50 +0200

    cors fix- 2

--- next.config.mjs
+++ next.config.mjs
@@ -7,9 +7,27 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const nextConfig = {
-  async rewrites() {
-    return [];
+  async headers() {
+    return [
+      {
+        // співставлення всіх API-маршрутів
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          { key: 'Access-Control-Allow-Origin', value: '*' }, // замініть це на список довірених доменів, з яких можна зробити запити
+          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
   },
+  // async rewrites() {
+  //   return [];
+  // },
   reactStrictMode: true,
   images: {
     remotePatterns: [
