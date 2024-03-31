287f9669e4502525fb7cd843291d0f8c4546787c
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-03-31 14:43:06 +0300

    fix cors 8

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,11 @@ const nextConfig = {
   async headers() {
     return [
       {
-        // співставлення всіх API-маршрутів
         source: '/api/:path*',
         headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' }, // замініть це на список довірених доменів, з яких можна зробити запити
-          { key: 'Access-Control-Allow-Methods', value: 'GET,DELETE,PATCH,POST,PUT' },
-          {
-            key: 'Access-Control-Allow-Headers',
-            value:
-              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-          },
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,PUT,DELETE,OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
         ],
       },
     ];
