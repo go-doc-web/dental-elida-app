2186cd707158756f4a485d9bb4419e28049d7d7f
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-03-31 17:57:01 +0300

    create console.log  test-ficha

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Banner from '@/compositions/Banner';
 import GallaryOfPage from '@/compositions/GallaryOfPage';
 
 export default function Home() {
+  console.log('test ficha/ git lesson');
   return (
     <>
       <Banner />
