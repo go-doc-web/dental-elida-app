311bf91bb3dde930e11d3722c542151ef17f81ae
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-03-31 14:03:08 +0300

    fix cors-5

--- src/api/requests/getUserProfile.js
+++ src/api/requests/getUserProfile.js
@@ -1,6 +1,7 @@
 'use client';
 import { api } from '../api';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default ({ router }) =>
   async dispatch => {
     const token = localStorage.getItem('token');
