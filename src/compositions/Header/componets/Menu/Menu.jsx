cfb00e6d82d2c47c2051a132a50bc8ec96b6d4b3
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-04-06 15:26:25 +0300

    create burger menu logic

--- src/compositions/Header/componets/Menu/Menu.jsx
+++ src/compositions/Header/componets/Menu/Menu.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 
 import Hamburger from '@/componets/Icon/Hamburger';
 import MenuItem from './MenuItem';
+import BurgerMenu from '@/componets/BurgerMenu';
 import useViewportWidth from '@/hooks/useViewportWidth';
 
 import css from './Menu.module.css';
@@ -10,20 +11,18 @@ import css from './Menu.module.css';
 function Menu({ menuItems }) {
   const { isEqualWidth } = useViewportWidth({ expect: 1140 });
   return (
-    <nav className={css.nav}>
-      {isEqualWidth && (
-        <button type="button">
-          <Hamburger color={'white'} />
-        </button>
-      )}
-      {!isEqualWidth && (
-        <ul className={css.navList}>
-          {menuItems.map(item => {
-            return <MenuItem key={item.id} item={item} />;
-          })}
-        </ul>
-      )}
-    </nav>
+    <>
+      <nav className={css.nav}>
+        {isEqualWidth && <BurgerMenu />}
+        {!isEqualWidth && (
+          <ul className={css.navList}>
+            {menuItems.map(item => {
+              return <MenuItem key={item.id} item={item} />;
+            })}
+          </ul>
+        )}
+      </nav>
+    </>
   );
 }
 
