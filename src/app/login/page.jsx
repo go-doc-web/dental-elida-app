8fbd3af9b03a165428255f530b9b6fb783446a75
Oleh Hubskiy <gubskyolegod@gmail.com>
2024-05-02 01:07:53 +0300

    fix form login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,23 +1,31 @@
 'use client';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import css from './Auth.module.css';
 import LoginForm from './LoginForm';
 
 const Login = () => {
   const route = useRouter();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const active = localStorage.getItem('isActive');
     if (active) {
+      setLoading(true);
       route.push('/reviews-management');
+      setLoading(false);
     }
   }, [route]);
 
   return (
     <>
-      <div className={css.wrapper}>
-        <LoginForm />
+      <div className={css.page}>
+        {loading && <p>Loading...</p>}
+        {!loading && (
+          <div className={css.wrapper}>
+            <LoginForm />
+          </div>
+        )}
       </div>
     </>
   );
